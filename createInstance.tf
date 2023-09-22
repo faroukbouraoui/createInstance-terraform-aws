cd0b549cf9a7d3ae412ea4f4585f62971a6b410c
faroukbouraoui <faroukbr050@gmail.com>
2023-09-22 11:23:47 +0100

    install docker with remote exec

--- createInstance.tf
+++ createInstance.tf
@@ -1,9 +1,35 @@
 resource "aws_instance" "trainingInstances" {
-    ami = "	ami-04b3c39a8a1c62b76"
-    instance_type = "t2.micro"
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.small"
+    security_groups = [ "launch-wizard-1" ]
+    key_name = "terraform"
+  
 
     tags = {
-      "Name" = "demoinstance"
+      "Name" = "docker instance"
     }
   
+}
+resource "null_resource" "docker_install" {
+  connection {
+    type        = "ssh"
+    user        = "ubuntu" # Change if necessary
+    private_key = file("C:\\Formations\\terraform.pem") # Change to your SSH key path
+    host        = aws_instance.trainingInstances.public_ip
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+    "sudo apt-get remove docker docker-engine docker.io",
+    "sudo apt-get update",
+    "sudo apt -y install docker.io",
+    "sudo snap -y install docker",
+    "sudo systemctl start docker"
+    ]
+  }
+  depends_on = [aws_instance.trainingInstances]
+}
+
+output "public_ip" {
+  value = aws_instance.trainingInstances.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,20 +1,15 @@
 variable "AWS_ACCESS_KEY" {
-  
+   default = ""
 }
 
 variable "AWS_SECRET_KEY" {
-  
+  default = ""
 }
 
 variable "AWS_SESSION_TOKEN" {
-  
+  default = ""
 }
 
 variable "AWS_REGION" {
   default = "us-east-1"
-}
-variable "Security_Group" {
-  type = "list"
-  default = ["sg-24076","sg-35214","sg-96824587"]
-  
 }
